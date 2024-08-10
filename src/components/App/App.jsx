d8ad197a04d195c4a2992f00ed7e57bab1c913cc
Meleah Roe <melroe19@gmail.com>
2024-08-10 04:38:14 -0500

    fetching list

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,7 +1,18 @@
-import {useState} from 'react';
+import {useEffect, useState} from 'react';
+import axios from 'axios';
 
 function App () {
-  
+  const [toDoList, setToDoList] = useState ([]);
+  const [newTask, setNewTask] =useState ('');
+  const [completed, setCompleted] = useState ('');
+
+useEffect{() +> {
+  fetchToDoList();
+
+}, []}
+
+const fetchToDoList = () +> {}
+
   return (
     <div>
       <h1>TO DO APP</h1>
